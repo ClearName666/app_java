d0b286aa60b887189eb3d8d92d75680be104d2a9
ClearName666 <pavelstorozev522@gmail.com>
2024-03-21 02:04:39 +0800

    post get запросы + wed

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -3,12 +3,17 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SecondActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,11 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Button buttonToThird = findViewById(R.id.buttonToThird);
+        Button buttonToGoWeb = findViewById(R.id.buttonGoWedView);
         TextInputEditText inputText = findViewById(R.id.inputTextSecond);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
 
         DatabaseTextInput db = new DatabaseTextInput(this);
         String text = db.getText(ConstsSettings.SecondInputTextIndexDataBaseText);
@@ -32,5 +41,22 @@ public class SecondActivity extends AppCompatActivity {
                 DataBases.saveToTextInputDataBase(inputText, SecondActivity.this, ConstsSettings.SecondInputTextIndexDataBaseText);
             }
         });
+        buttonToGoWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                WebView webView = (WebView) findViewById(R.id.webview);
+
+                WebSettings webSettings = webView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                webView.setWebViewClient(new WebViewClient());
+                TextInputEditText inputURLWeb = findViewById(R.id.inputURLWeb);
+                webView.loadUrl(inputURLWeb.getText().toString());
+
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+
     }
 }
